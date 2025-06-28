9912677967fb51d2e51a4173526f6ed6cac23000
Iman <imanyasishpro@gmail.com>
2025-06-28 11:46:58 +0530

    concerts view page added

---
+++ src/app/concerts/concerts-grid/view-concert/view-concert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-concert',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-concert.component.html',
+  styleUrl: './view-concert.component.css'
+})
+export class ViewConcertComponent {
+
+}
